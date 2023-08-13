053316377dcf0aaea4d68a258f5783d261924d54
mk1-p <mk1.dev662@gmail.com>
2023-08-13 14:51:08 +0900

    ENH: 캐릭터 카드 코드 샘플

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,    // 디버그 태그 이미지 제거
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,  // 중앙 정렬 여부
+        elevation: 0.0,     // AppBar 단차 효과 정도를 나타냄
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(  // Padding Widget 설정
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0), //가장자리의 상하좌우 패딩
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,   // 가로 시작점 정렬
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Center(
+              child: CircleAvatar(    // 원형 위젯
+                backgroundImage: AssetImage('assets/drink_ghost.gif'),
+                radius: 60.0,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Divider(          // 구분 선
+              height: 60.0,   // divider 위,아래의 간격을 나타냄!
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
             ),
+            Text('NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0    // 자간
+              ),
+            ),
+            SizedBox(       // 박스간 간격
+              height: 10.0,
+            ),
+            Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold   // 폰트 굵기
+              ),
+            ),
+            SizedBox(       // 박스간 간격
+              height: 30.0,
+            ),
+            Text('BBANTO POWER LEVEL',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0    // 자간
+              ),
+            ),
+            SizedBox(       // 박스간 간격
+              height: 10.0,
+            ),
+            Text('14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold   // 폰트 굵기
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(    // 가로로 나열할때 사용하는 위젯
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                )
+              ],
+            ),
+            Row(    // 가로로 나열할때 사용하는 위젯
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                )
+              ],
+            ),
+            Row(    // 가로로 나열할때 사용하는 위젯
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                )
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/common_ghost.png'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+// 사용 이미지 출처!
+// <a href="https://www.flaticon.com/kr/free-icons/" title="유령 아이콘">유령 아이콘  제작자: Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/kr/free-icons/" title="유령 아이콘">유령 아이콘  제작자: Smashicons - Flaticon</a>
+// <a href="https://pixabay.com/ko//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=4384">Pixabay</a>에서 <a href="https://pixabay.com/ko/users/aysugok90-21940915/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=4384">Aysu gök</a>님이 제공한 GIF
